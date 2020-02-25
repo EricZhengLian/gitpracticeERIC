5af67b8c40f59c523a7838aff4820d8432d82374
Miawang0206 <mingya.wang@hotmail.com>
2020-02-25 15:52:58 -0800

    add multiplyRandom function

--- practiceERIC.py
+++ practiceERIC.py
@@ -1,25 +1,27 @@
 #Partner 1: Eric
 #Partner 2: Mia
 ########################
-<<<<<<< HEAD
 import random
-=======
 #Assignment Name: GitHub Practice
 
 def getNRandom(n):
     '''takes in an integer and returns a list of n random integers between 1 and 10, inclusive'''
-    if n < 10:
+    if n < 11:
         return random.sample(range(1,11), n)
     else:
         return False
-        pass
+
 
 def multiplyRandom(numbers):
-	'''takes in a list of n numbers and returns the product of the numbers'''
-    pass
+    '''takes in a list of n numbers and returns the product of the numbers'''
+    product = 1
+    for i in numbers:
+        product = product * i
+    return product
+
 
 def main():
-	print(multiplyRandom(getNRandom(10))
+    print(multiplyRandom(getNRandom(10)))
 
 if __name__ == "__main__":
-	main()
+    main()
